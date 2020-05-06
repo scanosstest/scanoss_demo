7af47cc502f94dd37d183f2ad7dd091daada2d45
scanosstest <58334922+scanosstest@users.noreply.github.com>
2020-05-06 22:39:56 +0200

    Update InstanceManager.java

--- InstanceManager.java
+++ InstanceManager.java
@@ -48,4 +48,31 @@ public interface InstanceManager {
     default void backgroundProcess() {
         // NO-OP by default
     }
+    
+    @Override
+	@SuppressWarnings( {"unchecked"})
+	public synchronized void destroy() {
+		if ( active.compareAndSet( true, false ) ) {
+			try {
+				//First thing, make sure that the fast path read is disabled so that
+				//threads not owning the synchronization lock can't get an invalid Service:
+				initializedServiceByRole.clear();
+				synchronized (serviceBindingList) {
+					ListIterator<ServiceBinding> serviceBindingsIterator = serviceBindingList.listIterator(
+							serviceBindingList.size()
+					);
+					while ( serviceBindingsIterator.hasPrevious() ) {
+						final ServiceBinding serviceBinding = serviceBindingsIterator.previous();
+						serviceBinding.getLifecycleOwner().stopService( serviceBinding );
+					}
+					serviceBindingList.clear();
+				}
+				serviceBindingMap.clear();
+			}
+			finally {
+				parent.deRegisterChild( this );
+			}
+		}
+	}
+
 }
