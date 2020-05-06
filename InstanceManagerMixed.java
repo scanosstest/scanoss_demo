b410a7fdd544bfb5cad7dc66821a7fce8c51667d
scanosstest <58334922+scanosstest@users.noreply.github.com>
2020-05-06 22:45:16 +0200

    Update InstanceManagerMixed.java

--- InstanceManagerMixed.java
+++ InstanceManagerMixed.java
@@ -49,31 +49,37 @@ public interface InstanceManagerMixed {
         // NO-OP by default
     }
 	
-    @Override
-	@SuppressWarnings( {"unchecked"})
-	public synchronized void destroy() {
-		if ( active.compareAndSet( true, false ) ) {
-			try {
-				//First thing, make sure that the fast path read is disabled so that
-				//threads not owning the synchronization lock can't get an invalid Service:
-				initializedServiceByRole.clear();
-				synchronized (serviceBindingList) {
-					ListIterator<ServiceBinding> serviceBindingsIterator = serviceBindingList.listIterator(
-							serviceBindingList.size()
-					);
-					while ( serviceBindingsIterator.hasPrevious() ) {
-						final ServiceBinding serviceBinding = serviceBindingsIterator.previous();
-						serviceBinding.getLifecycleOwner().stopService( serviceBinding );
-					}
-					serviceBindingList.clear();
-				}
-				serviceBindingMap.clear();
-			}
-			finally {
-				parent.deRegisterChild( this );
-			}
-		}
+
+}
+
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.service.internal;
+
+/**
+ * A service provided as-is.
+ *
+ * @author Steve Ebersole
+ */
+public class ProvidedService<R> {
+	private final Class<R> serviceRole;
+	private final R service;
+
+	public ProvidedService(Class<R> serviceRole, R service) {
+		this.serviceRole = serviceRole;
+		this.service = service;
 	}
 
+	public Class<R> getServiceRole() {
+		return serviceRole;
+	}
+
+	public R getService() {
+		return service;
+	}
 }
 
